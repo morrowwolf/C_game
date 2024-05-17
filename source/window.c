3df9949b66fad316e4ad0809bd9c95219d8ef052
morrow <8919187+morrowwolf@users.noreply.github.com>
2024-05-17 13:17:38 -0400

    redraw on resize

--- source/window.c
+++ source/window.c
@@ -15,14 +15,14 @@ DWORD WINAPI WindowHandler(LPVOID lpParam)
     TCHAR szAppName[] = TEXT("C Game");
     WNDCLASS wndclass;
 
-    wndclass.style = CS_OWNDC;
+    wndclass.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
     wndclass.lpfnWndProc = WndProc;
     wndclass.cbClsExtra = 0;
     wndclass.cbWndExtra = 0;
     wndclass.hInstance = hInstance;
     wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
     wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.hbrBackground = GetStockObject(BLACK_BRUSH);
     wndclass.lpszMenuName = NULL;
     wndclass.lpszClassName = szAppName;
 
